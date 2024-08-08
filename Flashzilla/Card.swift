2c57e9d2d25547a0b265454b537e89c3aec2ec5e
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-08-08 14:43:52 -0600

    Initial Commit

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Ricardo on 08/08/24.
+//
+
+import Foundation
---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Ricardo on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
